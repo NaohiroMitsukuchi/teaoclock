587cbbdb8682aefc3198ab713f26597e5d0fe33f
Naohiro Mitsukuchi <bump11nao@gmail.com>
2021-07-04 23:16:43 +0900

    startBtn and resetBtn function

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -4,30 +4,41 @@ $(document).on('turbolinks:load', function(){
   const resetBtn  = $(".ButtonArea__reset");
   const detailBtn = $(".ButtonArea__detail");
   let timerMin    = $('.CountDownArea__timertext--min').text();
-  // let elapsedTime = 0;
+  let elapsedTime = 0;
+  let resetMin    = $('.CountDownArea__timertext--min').text();
+  let intervalId; 
 
   // タイマーの書き換えの関数
-  const rewiteTimer = (TimerId) => {
-    timerMin --;
+  const rewiteTimer = (intervalId, elapsedTime) => {
+    timerMin = resetMin - elapsedTime;
     if (timerMin < 0){
-      clearInterval(TimerId);
+      clearInterval(intervalId);
     }else{
       $('.CountDownArea__timertext--min').text(timerMin);
     }
   }
 
-// ボタンクリックの挙動
+// ボタンクリック時の挙動
   // カウントダウン開始
   startBtn.on('click', function(){
-    const TimerId = setInterval(() => {
-      rewiteTimer(TimerId);
-    }, 1000);
+    // $('.CountDownArea__timertext--min').disabled = true;
+    if(intervalId == null){
+      // intervalId = 0;
+      intervalId = setInterval(() => {
+        elapsedTime ++;
+        rewiteTimer(intervalId, elapsedTime);
+      }, 1000);
+    };
   });
 
   
   // タイマーリセット
   resetBtn.on('click', function(){
-    console.log("reset");
+    clearInterval(intervalId);
+    elapsedTime = 0;
+    intervalId = null;
+    $('.CountDownArea__timertext--min').text(resetMin);
+
   });
   // 詳細設定
   detailBtn.on('click', function(){
