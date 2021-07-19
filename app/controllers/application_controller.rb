cf90035bdedcb6d3b58ab2612f5f4e055a2c963b
Naohiro Mitsukuchi <bump11nao@gmail.com>
2021-07-19 23:11:10 +0900

    simple implementation sign_up, log_in and log_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_sign_up_params, if: :devise_controller?
+
+  protected
+  def configure_sign_up_params
+    added_attrs = [:nickname,:email,:encrypted_password]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys:  added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys:  added_attrs
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :email, uniqueness: {case_sensitive: true}, format: { with: /\A([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+\z/ }
 end
