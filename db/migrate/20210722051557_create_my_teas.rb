8a729a3bd7d5ff55218a0803d793ffaaa4830f94
Naohiro Mitsukuchi <bump11nao@gmail.com>
2021-07-22 15:07:49 +0900

    run db:migrate

--- db/migrate/20210722051557_create_my_teas.rb
+++ db/migrate/20210722051557_create_my_teas.rb
@@ -1,11 +1,11 @@
 class CreateMyTeas < ActiveRecord::Migration[6.0]
   def change
     create_table :my_teas do |t|
-      t.strings :product_name, null: false 
-      t.strings :campany 
-      t.references :tea_type, null: false, foreign_key: true 
-      t.strings :origin
-      t.strings :flavor
+      t.string :product_name, null: false 
+      t.string :campany 
+      t.integer :tea_type_id, null: false
+      t.string :origin
+      t.string :flavor
       t.references :user, null: false, foreign_key: true 
       t.timestamps
     end
