14634d90024387f9aecba04209a4957d63dce931
Naohiro Mitsukuchi <bump11nao@gmail.com>
2021-07-29 17:24:52 +0900

    able to registrate drop condition

--- app/controllers/drop_conditions_controller.rb
+++ app/controllers/drop_conditions_controller.rb
@@ -1,16 +1,18 @@
 class DropConditionsController < ApplicationController
 
   def new
+    @my_tea = MyTea.find(params[:my_tea_id])
     @drop_condition = DropCondition.new
-    # binding.pry
+    @leaf_types = LeafType.all
+    @tea_types = TeaType.all
   end
 
   def create
     @drop_condition = DropCondition.new(drop_condition_params)
-    if @drop_condition
+    if @drop_condition.save
       redirect_to my_teas_path
     else
-      render :new
+      render root_path
     end
   end
 
@@ -22,7 +24,7 @@ class DropConditionsController < ApplicationController
 
   private
   def drop_condition_params
-    params.require(:drop_condition).permit(:time, :tea_type_id, :number_of_people, :temperature, :note, :evalution, :my_tea_id).merge(user_id: current_user.id)
+    params.require(:drop_condition).permit(:time, :tea_type_id, :number_of_people, :temperature, :note, :evalution).merge(my_tea_id: params[:my_tea_id])
   end
 end
 
